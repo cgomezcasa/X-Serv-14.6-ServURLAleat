a4d0487d7898c1427f67737e7f4995b5fe13a583
cgomezcasa <cayegomez1995@gmail.com>
2018-03-02 09:58:29 +0100

    Archivo: aleat.py v3

--- aleat.py
+++ aleat.py
@@ -3,10 +3,11 @@
 import webapp
 import random
 
+
 class urlAleatApp(webapp.webApp):
     def process(self, parsedRequest):
-        return ("200 OK", "<html><body><h1><a href =" + 
-                str(random.randint(1, 1000000)) + 
+        return ("200 OK", "<html><body><h1><a href =" +
+                str(random.randint(1, 1000000)) +
                 "> Dame otra!</a></h1></body></html>")
 
 if __name__ == "__main__":
